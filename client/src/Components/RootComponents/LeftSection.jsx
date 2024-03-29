7f4fccb32c6a07573f0e4bacf7e2df0a00a2b64a
Moemen Adam <mmuummnna@gmail.com>
2024-03-30 01:34:34 +0200

    change scroll bar

--- client/src/Components/RootComponents/LeftSection.jsx
+++ client/src/Components/RootComponents/LeftSection.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from "react";
 import { LiaSearchSolid } from "react-icons/lia";
 import Chat from "../UiComponents/Chat";
+import {motion} from 'framer-motion';
 
 
 export default function LeftSection({ className = "" }) { 
@@ -12,15 +13,29 @@ export default function LeftSection({ className = "" }) {
     inputFocus();
   }, []);
   return (
-    <div className={`border-r-2 border-background1 pt-5 sm:px-5 px-2 ${className}`}>
+    <div className={`border-r-2 border-background1 pt-5 sm:pl-5 pl-2 ${className}`}>
       <h1 className="text-4xl font-bold">Chats</h1>
-      <LiaSearchSolid onClick={inputFocus} className="relative top-12 left-3 cursor-text" />
+      <LiaSearchSolid onClick={inputFocus} className="relative top-7   left-3 cursor-text" />
       <input
         ref={inputRef}
-        className="truncate pl-10 py-2 rounded-lg mt-5   text-gray bg-[#383838] focus:border-b-primary border-b-[#BBBBBB] border-b-2  outline-none w-full border-0 bg-transparent font-bold placeholder-gray mb-5"
+        className="truncate pl-10 py-2 mb-5 rounded-lg text-gray bg-[#383838] focus:border-b-primary border-b-[#BBBBBB] border-b-2  outline-none w-full border-0 bg-transparent font-bold placeholder-gray"
         type="text" placeholder="Search or start a new chat"
       />
-      <Chat Img="https://picsum.photos/200/300" Title="John Doe asd asd aaa aaaa aaa aa" Message="This is a long text that will be continuously truncated until the last character when decreasing" Time="12:00 PM" Counter={10} />
+      <motion.div
+        initial={{ opacity: 0, y:10 }}
+        animate={{ opacity: 1, y:0 }}
+        className="h-[calc(100%-140px)] overflow-auto sm:pr-5 pr-2">
+        <Chat Img="https://picsum.photos/200/300" Title="SomeName" Message="hello World" Time="12:00 PM" Counter={10} />
+        <Chat Img="https://picsum.photos/200/300" Title="SomeName" Message="hello World" Time="12:00 PM" Counter={10} />
+        <Chat Img="https://picsum.photos/200/300" Title="SomeName" Message="hello World" Time="12:00 PM" Counter={10} />
+        <Chat Img="https://picsum.photos/200/300" Title="SomeName" Message="hello World" Time="12:00 PM" Counter={10} />
+        <Chat Img="https://picsum.photos/200/300" Title="SomeName" Message="hello World" Time="12:00 PM" Counter={10} />
+        <Chat Img="https://picsum.photos/200/300" Title="SomeName" Message="hello World" Time="12:00 PM" Counter={10} />
+        <Chat Img="https://picsum.photos/200/300" Title="SomeName" Message="hello World" Time="12:00 PM" Counter={10} />
+        <Chat Img="https://picsum.photos/200/300" Title="SomeName" Message="hello World" Time="12:00 PM" Counter={10} />
+        <Chat Img="https://picsum.photos/200/300" Title="SomeName" Message="hello World" Time="12:00 PM" Counter={10} />
+        <Chat Img="https://picsum.photos/200/300" Title="SomeName" Message="hello World" Time="12:00 PM" Counter={10} />
+      </motion.div>
     </div>
   )
 }
--- client/src/Components/UiComponents/Chat.jsx
+++ client/src/Components/UiComponents/Chat.jsx
@@ -4,7 +4,6 @@ import Person from '../../assets/Person.svg';
 export default function Chat({ Img, Title, Message, Time, Counter }) {
   const [Width, setWidth] = useState(window.innerWidth>=768?100:100);
 
-  console.log(Width);
   useEffect(() => {
     const handleResize = () => {
       setWidth(window.innerWidth>=768?100:100);
@@ -17,17 +16,18 @@ export default function Chat({ Img, Title, Message, Time, Counter }) {
   },[])
   return (
     <div className="flex gap-5 hover:bg-[#383838] p-2 rounded-lg select-none">
-      <div className="min-w-[60px] h-[60px] flex justify-center items-center rounded-full bg-[#414141]">
-        <img className="w-2/3" src={Person} alt="" />
+      <div className="min-w-[60px] h-[60px] flex justify-center items-center rounded-full overflow-hidden bg-[#414141]" style={{background:Person}}>
+        <img className="w-full h-full" src={Img} alt="" />
+        {/* <img className="w-2/3 h-full" src={Person} alt="" /> */}
       </div>
       <div className="flex flex-grow gap-2 relative overflow-hidden">
         <div className={`flex flex-col justify-between overflow-hidden`}>
-          <div className='mytruncate' title={Title}>{Title}</div>
+          <div className='mytruncate font-bold' title={Title}>{Title}</div>
           <div className='mytruncate text-[#BBBBBB]' title={Message}>{Message}</div>
         </div>
         <div className="flex flex-col justify-between items-end absolute right-0 rounded-xl w-[80px] z-10  h-full">
-          <div className="text-primary w-[70px]">{Time}</div>
-          <div className="rounded-full text-center bg-primary text-black w-[40px] ">
+          <div className="text-primary w-[71px] font-bold">{Time}</div>
+          <div className="rounded-full text-center font-bold bg-primary text-black w-[40px] ">
             {Counter > 99 ? "99+" : Counter}
           </div>
         </div>
