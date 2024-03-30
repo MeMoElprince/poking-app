feccaaeb41e55865ff7c3e421773652598a46c6e
Mustafa Elsharawy <memomeme621@gmail.com>
2024-03-30 03:23:41 +0200

    establishing basic structure

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+(
+    async () => {
+        try {
+            console.log('trying to Connect to <whosapp> database');
+            await mongoose.connect(DB);
+            console.log('Database connected successfully...');
+        } catch (err) {
+            console.log('Error connecting to database...');
+            console.log(err.message);
+        }
+    }
+)();
+
+const port = process.env.PORT || 2000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
+
