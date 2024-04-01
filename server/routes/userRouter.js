a2df9384f512b92ffe2895f630b97441cdd18b82
Mustafa Elsharawy <memomeme621@gmail.com>
2024-04-01 21:19:13 +0200

    creates update getMyData GetUserData Routes

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -10,8 +10,9 @@ router.post('/verify', authController.verifyEmail);
 router.post('/confirm', authController.confirmEmail);
 
 
-
 router.patch('/updateMe', authController.protect, userController.updateMe);
+router.get('/me', authController.protect, userController.getMe);
+router.get('/unique', userController.getUser);
 
 
 
