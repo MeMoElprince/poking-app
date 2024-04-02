f7e13961031d4d45ee7cd4905951e6648370de48
Mustafa Elsharawy <memomeme621@gmail.com>
2024-04-02 16:18:52 +0200

    more routes

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -8,14 +8,13 @@ const router = route.Router();
 
 router.post('/verify', authController.verifyEmail);
 router.post('/confirm', authController.confirmEmail);
-
-
+router.patch('/logout', authController.protect, authController.logout);
 
 router.patch('/addFriend/:id', authController.protect, userController.addFriend);
+router.patch('/deleteFriend/:id', authController.protect, userController.deleteFriend);
 router.patch('/acceptFriend/:id', authController.protect, userController.acceptFriend);
 router.patch('/declineFriend/:id', authController.protect, userController.declineFriend);
 router.patch('/updateMe', authController.protect, userController.updateMe);
-
 
 router.get('/me', authController.protect, userController.getMe);
 router.get('/unique/:email', userController.getUser);
