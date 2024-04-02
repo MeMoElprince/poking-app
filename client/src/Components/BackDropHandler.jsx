8b44eb4b8a50cc86b2437fd83251f4bb7b712811
Moemen Adam <mmuummnna@gmail.com>
2024-04-02 19:17:39 +0200

    finish logout

--- client/src/Components/BackDropHandler.jsx
+++ client/src/Components/BackDropHandler.jsx
@@ -1,22 +1,45 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import Root from "./Root"
 import { BackDropCtx } from '../Store/Context/BackDropContext'
 import { UserAuthCtx } from '../Store/Context/UserAuthContext'
 import { motion } from 'framer-motion'
+import { LogOut } from '../Store/urls'
+import LoadingSpinner from './UiComponents/LoadingSpinner'
 
-const LogOutBackDrop = ({ setBackDropType,setLogedIn }) => {
-  const handleClick = (type) => {
-    if(type==='Confirm'){
-      // Log out
+const LogOutBackDrop = ({ setBackDropType, setLogedIn, Loading,setLoading }) => {
+  const { Token } = useContext(UserAuthCtx);
+  const LogOutFetch = async () => {
+    const url = LogOut();
+    setLoading(true)
+    try {
+      const response = await fetch(url, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${Token}`,
+        },
+      });
+      const res = await response.json();
+      console.log({ res });
+      setLoading(false)
       setLogedIn(false);
-    }else{
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  const handleClick = (type) => {
+    if(Loading)return;
+    if (type === 'Confirm') {
+      // Log out
+      LogOutFetch();
+    } else {
       setBackDropType('Root')
     }
   }
   return (
     <motion.div
-      initial={{ opacity: 0, y: 0 , x:'-50%' }}
-      animate={{ opacity: 1, y: '-50%' , x:'-50%' }}
+      initial={{ opacity: 0, y: 0, x: '-50%' }}
+      animate={{ opacity: 1, y: '-50%', x: '-50%' }}
       className='absolute top-1/2 left-1/2 z-50
         bg-black w-full sm:w-[500px]
         flex flex-col justify-between gap-10
@@ -24,12 +47,16 @@ const LogOutBackDrop = ({ setBackDropType,setLogedIn }) => {
       '>
       <h1 className='text-xl'>Are you sure you want to log out ?</h1>
       <div className='flex justify-around w-full flex-wrap gap-2'>
-        <button className='w-[140px] bg-primary py-3 rounded-lg hover:scale-105 duration-150' onClick={()=>{handleClick('Confirm')}}>
-          Log out
-        </button>
-        <button className='w-[140px] bg-background2 py-3 rounded-lg hover:scale-105 duration-150' onClick={()=>{handleClick('Cancle')}}>
-          Cancle
-        </button>
+        <div className='relative'>
+          <button className={`w-[140px] bg-primary py-3 rounded-lg hover:scale-105 duration-150 ${Loading ?'pointer-events-none opacity-20':''}`} onClick={() => { handleClick('Confirm') }}>
+            Log out
+          </button>
+        </div>
+        <div className='relative'>
+          <button className={`w-[140px] bg-background2 py-3 rounded-lg hover:scale-105 duration-150 ${Loading ?'pointer-events-none opacity-20':''}`} onClick={() => { handleClick('Cancle') }}>
+            Cancle
+          </button>
+        </div>
       </div>
     </motion.div>
   )
@@ -38,17 +65,19 @@ const LogOutBackDrop = ({ setBackDropType,setLogedIn }) => {
 export default function BackDropHandler() {
   const { BackDropType, setBackDropType } = useContext(BackDropCtx);
   const { setLogedIn } = useContext(UserAuthCtx);
+  const [Loading,setLoading] = useState(false);
 
   const Style = BackDropType !== 'Root' ? 'pointer-events-none select-none' : '';
   const handleClick = () => {
-    if (BackDropType !== 'Root') 
+    if(Loading)return;
+    if (BackDropType !== 'Root')
       setBackDropType('Root')
   }
   return (
     <>
-      {BackDropType === 'UserLogo' && <LogOutBackDrop setLogedIn={setLogedIn} setBackDropType={setBackDropType} />}
+      {BackDropType === 'UserLogo' && <LogOutBackDrop  Loading={Loading} setLoading={setLoading} setLogedIn={setLogedIn} setBackDropType={setBackDropType} />}
       <div onClick={handleClick}>
-        <div className={`${BackDropType !== 'Root'?"brightness-[0.2]":""} ${Style}`}>
+        <div className={`${BackDropType !== 'Root' ? "brightness-[0.2]" : ""} ${Style}`}>
           <Root />
         </div>
       </div>
