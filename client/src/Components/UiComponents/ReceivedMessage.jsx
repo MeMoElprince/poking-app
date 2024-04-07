ed7523c6b0498bc00e788f1991b6ebf9c7bbd94a
Moemen Adam <mmuummnna@gmail.com>
2024-04-08 01:29:28 +0200

    fix arabic and eng text problem when being together

--- client/src/Components/UiComponents/ReceivedMessage.jsx
+++ client/src/Components/UiComponents/ReceivedMessage.jsx
@@ -1,7 +1,7 @@
 
 export default function ReceivedMessage({message,customeStyle}) {
   return (
-    <div className={`bg-background1 w-fit py-2 px-10 rounded-lg relative ${customeStyle}`}>
+    <div className={`bg-background1 w-fit py-2 px-10 rounded-lg relative ${customeStyle}`} style={{direction:'rtl'}}>
       <div className="absolute top-0 left-0 w-0 h-0 border-solid border-transparent border-r-8 border-t-8 bg-background1 break-all"></div>
       {message}
     </div>
