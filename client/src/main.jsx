e3ce0a0e21ebc035489b8c4b485f30426e6498b1
Moemen Adam <mmuummnna@gmail.com>
2025-03-23 01:37:42 +0200

    removes: calculator and routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,24 +1,15 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import FirstPage from './FirstPage.jsx'
-import Calculator from './calculator/Calculator.jsx'
 import './index.css'
 import UserContext from './Store/Context/UserContext.jsx'
 import { Analytics } from "@vercel/analytics/react"
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <UserContext>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<FirstPage />} />
-        <Route path="/PokingApp" element={<App />} />
-        <Route path="/Calculator" element={<Calculator />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
     <ToastContainer />
     <Analytics />
   </UserContext>
